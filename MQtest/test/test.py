9688cbb1a10733a49162ec25792dafd4c481f1cf
S013k <serg@hayduk.ru>
2024-10-02 21:23:09 +0300

    Implement MQ setup for social rating project: Added .env configurations, Dockerfile and docker-compose for MQ service, and updated requirements and tests. (2024-10-02 21:23:09)

---
+++ MQtest/test/test.py
@@ -0,0 +1,24 @@
+import pika
+
+# Connect to RabbitMQ
+connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
+channel = connection.channel()
+
+# Declare queue
+channel.queue_declare(queue='test')
+
+# Publish message
+channel.basic_publish(exchange='', routing_key='test', body='Hello World!')
+print("Sent message")
+
+# Define callback for receiving messages
+def callback(ch, method, properties, body):
+    print("Received: %r" % body)
+
+# Set up subscription on queue
+channel.basic_consume(queue='test',
+                      auto_ack=True,
+                      on_message_callback=callback)
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
