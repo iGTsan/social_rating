5c9816f70db3865fa818ce2a256607e0e28297bb
vit_72 <Bogolyubov2003@bk.ru>
2024-10-08 18:24:28 +0300

    Implemented updates and fixes for daily service components in preparation for production deployment (2024-10-08 18:24:28)

--- main/DAILY_SERVICE/daily_exec.py
+++
@@ -1,7 +0,0 @@
-def dailyTasks(isProdigy, isLocal, sendQueue, pipeQueue, debug):
-    print("dailyTasks")
-    try:
-        tasks = daily.Daily(isProdigy, isLocal, sendQueue, pipeQueue, debug)
-        tasks.start()
-    except Exception as shit:
-        print("dailyTasks", shit)
