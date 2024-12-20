7ced8685f04c893eb5d8e16a3da18c4539b8d9d3
Ваше Имя <you@example.com>
2024-12-20 19:19:01 +0300

    Fix issues

--- set_secrets.sh
+++ set_secrets.sh
@@ -1,10 +1,10 @@
-echo "K0ntR0L3r" > POSTGRES_PW.txt
+head /dev/random | tr -dc 'A-Za-z0-9' | head -c 12 > POSTGRES_PW.txt
 # docker secret create POSTGRES_PW POSTGRES_PW.txt
 
-echo "f9dcbba9dbf53a69468f593fda7a6c6fa07d81ea8f6033bb90364bf3fc82ffe978b063d7e73be9d9ca235" > VK_API_TOKEN.txt
-echo "197929101" > VK_GROUP_ID.txt
+echo "your_vk_api_token" > VK_API_TOKEN.txt
+echo "your_group_id" > VK_GROUP_ID.txt
 # docker secret create VK_API_TOKEN VK_API_TOKEN.txt
 # docker secret create VK_GROUP_ID VK_GROUP_ID.txt
 
-echo "7670865123:AAHT3BIRonw4X0MILtGQzWF6A7HXbEMDOcg" > TG_API_TOKEN.txt
+echo "your_tg_api_token" > TG_API_TOKEN.txt
 # docker secret create TG_API_TOKEN TG_API_TOKEN.txt
